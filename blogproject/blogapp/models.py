157f1000708112ccf8c755ca3b30a44463d9d725
dilaraCerenMoral <dc.moral95@gmail.com>
2021-11-13 20:16:49 +0300

    post and comment models

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    objects = models.Manager()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=200)
+    last_edit_datetime = models.DateTimeField(null=True)
+    content = models.TextField()
+    total_views = models.BigIntegerField(default=0,null=False, blank=True)
+    image_name = models.CharField(max_length=100)
+
+
+class Comment(BaseModel):
+    nickname = models.CharField(max_length=24)
+    content = models.CharField(max_length=500)
+    mail = models.CharField(max_length=320)
+
+
+
+
